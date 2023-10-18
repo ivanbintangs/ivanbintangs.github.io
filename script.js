2a019714e7fc00bc384ce31ef15c101d38fcd63c
ivanbintangs <110510815+ivanbintangs@users.noreply.github.com>
2023-10-18 23:12:25 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle input");
-    const nav = document.querySelector("nav ul");
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('');
 
-    menuToggle.addEventListener("click", function () {
-        nav.classList.toggle("active");
-    });
-
-    // Close the navigation menu when a link is clicked
-    const navLinks = document.querySelectorAll("nav ul li a");
-
-    navLinks.forEach(link => {
-        link.addEventListener("click", function () {
-            nav.classList.remove("active");
-        });
-    });
+menuToggle.addEventListener('click', function () {
+  nav.classList.toggle('slide');
 });
